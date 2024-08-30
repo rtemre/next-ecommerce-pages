3e2454fa9946f63b774727959f56467ae2c67a5d
Raman Temre <ramantemre2110@gmail.com>
2024-08-30 12:54:41 +0530

    feat: MailChimp Integration for Subscribe

--- components/subscribe/index.tsx
+++ components/subscribe/index.tsx
@@ -1,26 +1,45 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import toast from "react-hot-toast";
+import jsonp from "jsonp";
+import Loader from "../loader";
 
 const Subscribe = () => {
+  const [isLoading, setIsLoading] = useState(false);
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
     const email = formData.get("email");
-    const response = await fetch("/api/subscribe", {
-      method: "POST",
-      body: JSON.stringify({ email }),
-      headers: {
-        "Content-Type": "appliction/json",
-      },
-    });
+    // const response = await fetch("/api/subscribe", {
+    //   method: "POST",
+    //   body: JSON.stringify({ email }),
+    //   headers: {
+    //     "Content-Type": "appliction/json",
+    //   },
+    // });
 
-    if (response) {
-      const jsonData = response.json();
-      jsonData.then((data: any) =>
-        toast.success(data.message || "Successfully Subscribed!")
-      );
-    }
+    // if (response) {
+    //   const jsonData = response.json();
+    //   jsonData.then((data: any) =>
+    //     toast.success(data.message || "Successfully Subscribed!")
+    //   );
+    // }
+    setIsLoading(true);
+    const url = process.env.MAILCHIMP_URL;
+    jsonp(`${url}&EMAIL=${email}`, { param: "c" }, (_: any, data: any) => {
+      const { msg } = data;
+      if (data?.result === "error") {
+        if (msg?.includes(" - ")) {
+          toast.error(msg?.split(" - ")[1]);
+        } else {
+          toast.error(msg);
+        }
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        toast.success(msg || "Successfully Subscribed!");
+      }
+    });
   }
   return (
     <section className="subscribe">
@@ -40,8 +59,12 @@ const Subscribe = () => {
               name="email"
               required
             />
-            <button type="submit" className="btn btn--rounded btn--yellow">
-              Subscribe
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="btn btn--rounded btn--yellow"
+            >
+              {isLoading ? <Loader /> : "Subscribe"}
             </button>
           </form>
         </div>
